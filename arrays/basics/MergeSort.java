c0e15ba6c36e3da031769fc826a4d064f49b519f
Vamsi Krishna <iam.vamsitupakula@gmail.com>
2025-12-27 13:30:20 +0530

    chore: add merge sort

---
+++ arrays/basics/MergeSort.java
@@ -0,0 +1,58 @@
+package arrays.basics;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    static void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void mergeSort(int[] arr, int start, int end) {
+        if (start >= end)
+            return;
+        int mid = start + (end - start) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 3, 4, 17, 23, 16 };
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
